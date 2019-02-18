db0cd2af7bf80ceb32cbb7e75312fa52272ecb72
Nicolas Essayan <nicolas_essayan@yahoo.fr>
2019-02-18 18:24:34 +0100

    Models & DB finally...

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+  has_many :bookings, dependent: :destroy
+  has_many :yachts, dependent: :destroy
+  validates :first_name, presence: true, uniqueness: { scope: :last_name }
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A.*@.*\.com\z/ }
 end
