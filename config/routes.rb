7975507108d52dcf33c156d95a7e6123bd31e1d1
GuillaumeLaganier <g.laganier@gmail.com>
2019-02-19 10:38:05 +0100

    right pundit gem install

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources: yachts
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
